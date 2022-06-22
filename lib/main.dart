57a1aead718ba5e77d8cc0c0e8304c16db55b824
ASUS <hardiksachdeva52@gmail.com>
2022-06-22 21:57:16 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -13,29 +13,48 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.blue,
         body: SafeArea(
-        child: Column(
-          verticalDirection: VerticalDirection.down,
-          children: <Widget>[
-          Container(
-          height: 100.0,
-          width: 200.0,
-          margin: EdgeInsets.fromLTRB(30.0, 10.0, 50.0, 20.0 ),
-          padding: EdgeInsets.all(20.0),
-          color: Colors.white,
-          child: Text('Container 1'),
-          ),
+            child: Column(
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('images/hardikk.jpg'),
+
+                ),
+                Text('hardikkk'),
+              ],
+            )),
+      ),
+    );
+  }
+         /* child:
+          Column(
+            verticalDirection: VerticalDirection.down,
+            children: <Widget>[
             Container(
             height: 100.0,
-            width: 200.0,
-              margin: EdgeInsets.fromLTRB(30.0, 10.0, 50.0, 20.0 ),
+            width: double.infinity,
+              margin: EdgeInsets.only(left:150.0),
               padding: EdgeInsets.all(20.0),
-            color: Colors.red,
-            child: Text('Container 2'),
-              ),
-          ],
-        ),
+            color: Colors.white,
+            child: Column(
+              children: [
+                Text  ('Container 1'),
+              ],
+            ),
+            ),
+              Container(
+              height: 100.0,
+              width: double.infinity,
+                padding: EdgeInsets.all(20.0),
+                margin: EdgeInsets.only(left:150.0),
+              color: Colors.amberAccent,
+              child: Text('Container 2'),
+                ),
+            ],
+          ),
+        ],
       ),
       ),
     );
   }
-}
+} */}
